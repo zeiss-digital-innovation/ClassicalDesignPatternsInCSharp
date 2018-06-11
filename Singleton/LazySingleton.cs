ac4e96f38f0b5171ab11f54e4408f31cffc06fa5
Michael Wresche <michael.wresche@googlemail.com>
2018-06-11 18:35:39 +0200

    Add thread-safe implementation of singleton pattern using the Lazy<T> class

---
+++ Singleton/LazySingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassicalDesignPatternsInCSharp.Singleton
+{
+    /// <summary>
+    ///     Encapsulates the implementation of the Singleton design pattern as described by the Gang of For (GoF).
+    /// </summary>
+    /// <remarks>
+    ///     The described solution is thread-safe due to the fact that <see cref="Lazy{T}" /> is thread-safe.
+    /// </remarks>
+    public class LazySingleton
+    {
+        private static readonly Lazy<LazySingleton> instance = new Lazy<LazySingleton>(() => new LazySingleton());
+
+        /// <summary>
+        ///     Prevents a default instance of the <see cref="LazySingleton" /> class from being created.
+        /// </summary>
+        private LazySingleton()
+        {
+        }
+
+        /// <summary>
+        ///     Gets the singleton instance of the <see cref="LazySingleton" /> class.
+        /// </summary>
+        public static LazySingleton Singleton => instance.Value;
+    }
+}
